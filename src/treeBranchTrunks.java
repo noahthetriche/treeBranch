046890f75515a39ba2e51234c70037f609bf19d0
Noah Triche <noahthetriche@Noahs-MacBook-Air.local>
2020-03-09 13:21:15 -0700

    Initial commit

---
+++ src/treeBranchTrunks.java
@@ -0,0 +1,9 @@
+public class treeBranchTrunks
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Trees die every day");
+    }//end main method
+
+}//end driver class
+//this is the base program, share on github and edit there
