34a4dd818ca9b15611a4063ca70aab0e1ec6c41b
noahthetriche <noahthetriche@gmail.com>
2020-03-09 13:29:09 -0700

    succulent, yes ma'am

--- src/treeBranchTrunks.java
+++ src/treeBranchTrunks.java
@@ -5,6 +5,7 @@ public class treeBranchTrunks
         System.out.println("Trees die every day");
         System.out.println("Mr. Beast planted 20,000,000 trees");
         System.out.println("There are many types of trees.");
+        System.out.println("Succulents are nice too.");
     }//end main method
 
 }//end driver class
