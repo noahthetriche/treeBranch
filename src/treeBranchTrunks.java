80f40d0381bd89f127283bdc989e0c146955534c
noahthetriche <noahthetriche@gmail.com>
2020-03-09 13:26:05 -0700

    mr beast's brag

--- src/treeBranchTrunks.java
+++ src/treeBranchTrunks.java
@@ -3,6 +3,7 @@ public class treeBranchTrunks
     public static void main(String [] args)
     {
         System.out.println("Trees die every day");
+        System.out.println("Mr. Beast planted 20,000,000 trees");
     }//end main method
 
 }//end driver class
